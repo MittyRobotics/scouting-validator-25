0953ec4b096445981db0b12deaae713f8cf3e834
skleenn <113645968+skleenn@users.noreply.github.com>
2024-06-29 21:44:31 -0700

    setup TBA API and wrote the calc_score function intending to use data from the api

---
+++ match.py
@@ -0,0 +1,25 @@
+import requests
+import fileinput
+import os
+import json
+
+matchnum = 77
+event_key = "2024casj"
+match_key = f"{event_key}_qm{matchnum}"
+url="https://www.thebluealliance.com/api/v3"
+url += f"/match/{match_key}"
+tba = requests.get(url, headers={"X-TBA-Auth-Key":"ingmesQTeprOTbKZRvexSACtqw6bSSdF5WLBiJdbdw38OQAtikq0Rz5MOIYFtR4z"})
+red_recorded = 0
+blue_recorded = 0
+bluescore = 0
+redscore = 0
+red_real = 0
+blue_real = 0
+data = json.loads(json.dumps(tba.json()))
+
+#function to calculate score from official API 
+def calc_score(autoAmp, autoSpe, teleAmp, teleSpe, teleSpeAmp, trap, park, onstage):
+        score = 0
+        teleSpe += teleSpeAmp
+        score += (autoAmp*2) + (autoSpe*5) + (teleAmp) + (teleSpe*2) + trap + park + onstage
+        return score
